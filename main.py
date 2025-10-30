7d1cea8a6b831486eefe2ed32d49f59c19610a26
drunkenjedi <wrightsh@gmail.com>
2025-10-29 23:18:51 -0600

    Added player as a triangle
    Added hitbox as a square for player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,7 +10,11 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
 
@@ -18,7 +24,10 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
